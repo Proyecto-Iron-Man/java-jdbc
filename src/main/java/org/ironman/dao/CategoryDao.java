e19b119f44a10f6a4b8bdcda1b32c0ec7a0f1263
AbelPalominoRojas <abel.palominorojas@gmail.com>
2023-07-15 20:25:20 -0500

    Use CrudCore

--- src/main/java/org/ironman/dao/CategoryDao.java
+++ src/main/java/org/ironman/dao/CategoryDao.java
@@ -1,23 +1,7 @@
 package org.ironman.dao;
 
-import org.ironman.core.ConnectionCore;
+import org.ironman.core.CrudCore;
 import org.ironman.entity.Category;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.util.ArrayList;
-import java.util.List;
-
-public interface CategoryDao {
-
-    List<Category> findAll() throws Exception;
-
-    Category findById(Long id) throws Exception;
-
-    Category create(Category category) throws Exception;
-
-    Category edit (Category category, Long id) throws Exception;
-
-    void remove(Long id) throws Exception;
+public interface CategoryDao extends CrudCore<Category, Long> {
 }
---
+++ src/main/java/org/ironman/dao/ProfileDao.java
@@ -0,0 +1,7 @@
+package org.ironman.dao;
+
+import org.ironman.core.CrudCore;
+import org.ironman.entity.Profile;
+
+public interface ProfileDao extends CrudCore<Profile, Long> {
+}
